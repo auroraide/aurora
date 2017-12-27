a666aec11892fdb02695521d83db5e3975950905
Randy Seng <uidzi@student.kit.edu>
2017-12-27 23:00:19 +0100

    Add DesktopView Layout

--- src/aurora/client/view/DesktopView.java
+++ src/aurora/client/view/DesktopView.java
@@ -1,4 +1,31 @@
 package aurora.client.view;
 
-public class DesktopView {
+import aurora.client.view.editor.Editor;
+import aurora.client.view.sidebar.Sidebar;
+import aurora.client.view.topmenu.TopMenu;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class DesktopView extends Composite{
+    interface DesktopViewUiBinder extends UiBinder<Widget, DesktopView> {
+    }
+
+    private static DesktopViewUiBinder ourUiBinder = GWT.create(DesktopViewUiBinder.class);
+
+    @UiField
+    TopMenu topMenu;
+
+    @UiField
+    Sidebar sidebar;
+
+    @UiField
+    Editor editor;
+
+    public DesktopView() {
+
+        initWidget(ourUiBinder.createAndBindUi(this));
+    }
 }
