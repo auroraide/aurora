d63081047f47817454f859e02c83c93aab7400e6
Randy Seng <uidzi@student.kit.edu>
2018-01-03 16:51:47 +0100

    Add EventHandler stubs  for StrategySelection

--- src/aurora/client/view/sidebar/components/strategy/StrategySelection.java
+++ src/aurora/client/view/sidebar/components/strategy/StrategySelection.java
@@ -1,8 +1,10 @@
 package aurora.client.view.sidebar.components.strategy;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.Widget;
@@ -20,5 +22,25 @@ public class StrategySelection extends Composite {
 
     public StrategySelection() {
         initWidget(ourUiBinder.createAndBindUi(this));
+    }
+
+    @UiHandler("callByValue")
+    void onCallByValueSelected(ClickEvent event) {
+
+    }
+
+    @UiHandler("callByName")
+    void onCallByNameSelected(ClickEvent event) {
+
+    }
+
+    @UiHandler("normalOrder")
+    void onNormalOrderSelected(ClickEvent event) {
+
+    }
+
+    @UiHandler("manualSelection")
+    void onManualSelectionSelected(ClickEvent event) {
+
     }
 }
