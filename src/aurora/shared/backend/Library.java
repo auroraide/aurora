05cde8bbc3eb213eea82c6f670ece612baac8df6
Max <max.nowak.j@gmail.com>
2018-01-04 15:48:20 +0100

    add Library.java

---
+++ src/aurora/shared/backend/Library.java
@@ -0,0 +1,10 @@
+package aurora.shared.backend;
+
+public class Library {
+    public Term getTerm(String name) {
+        return null;
+    }
+
+    public void defTerm(String name, Term term) {
+    }
+}
