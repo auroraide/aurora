c34d93a03d4a1209ea6958a9406761b3e5c7f12c
Mercur IP <mercurip@Iuliias-MacBook-Pro.local>
2018-01-05 18:06:26 +0100

    Sidebar add share and language listbox

--- src/aurora/client/view/sidebar/Sidebar.java
+++ src/aurora/client/view/sidebar/Sidebar.java
@@ -1,6 +1,8 @@
 package aurora.client.view.sidebar;
 
 import aurora.client.view.sidebar.components.library.AddFunctionDialogBox;
+import aurora.client.view.sidebar.components.share.ShareLaTeXDialogBox;
+import aurora.client.view.sidebar.components.language.selection.LanguageSelection;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -14,19 +16,37 @@ public class Sidebar extends Composite {
 
     private static SidebarUiBinder ourUiBinder = GWT.create(SidebarUiBinder.class);
     private final AddFunctionDialogBox addFunctionDialogBox;
+    private final ShareLaTeXDialogBox shareLaTeXDialogBox;
+    private final LanguageSelection languageSelection;
+    // change the active language to the chosen
 
     @UiField TextBox stepSetting;
     @UiField Button addFunction;
     @UiField ToggleButton nightModeSwitch;
+    @UiField ListBox shareList;
+    @UiField ListBox languageList;
 
     public Sidebar() {
-
         initWidget(ourUiBinder.createAndBindUi(this));
+        //init get languages fromt he language selection (check in properties)
         addFunctionDialogBox = new AddFunctionDialogBox();
+        shareLaTeXDialogBox = new ShareLaTeXDialogBox();
+        languageSelection = new LanguageSelection();
     }
 
     @UiHandler("addFunction")
     void onAddFunctionClicked(ClickEvent event) {
         addFunctionDialogBox.show();
+    }
+
+    @UiHandler("shareList")
+    void onShareOptionChosen(ClickEvent event) {
+        //what does the event have for info?
+        shareLaTeXDialogBox.show();
+    }
+
+    @UiHandler("languageList")
+    void onLangageOptionChosen(ClickEvent event) {
+
     }
 }
