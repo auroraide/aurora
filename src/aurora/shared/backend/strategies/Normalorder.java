b3e10ef7df35661d9e184e605bc654750c8e91a6
Max <max.nowak.j@gmail.com>
2018-01-05 16:38:57 +0100

    Rename

--- src/aurora/shared/backend/strategies/Normalorder.java
+++ src/aurora/shared/backend/strategies/Normalorder.java
@@ -3,7 +3,7 @@ package aurora.shared.backend.strategies;
 import aurora.shared.backend.TreePath;
 import aurora.shared.backend.tree.Term;
 
-public class Normalorder extends ReductionStrategy {
+public class NormalOrder extends ReductionStrategy {
     @Override
     public TreePath getRedex(Term t) {
         return null;
