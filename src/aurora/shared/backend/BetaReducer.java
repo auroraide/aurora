cb592830015947ea105704021d4a953e6fc920d2
Max <max.nowak.j@gmail.com>
2018-01-05 16:12:17 +0100

    Implement beta reduction

--- src/aurora/shared/backend/BetaReducer.java
+++ src/aurora/shared/backend/BetaReducer.java
@@ -1,15 +1,34 @@
 package aurora.shared.backend;
 
 import aurora.shared.backend.strategies.ReductionStrategy;
+import aurora.shared.backend.tree.Application;
 import aurora.shared.backend.tree.Term;
 
 public class BetaReducer {
-    ReductionStrategy strategy;
-    Term t;
-    public BetaReducer(ReductionStrategy strategy, Term t) {
-        this.strategy = strategy;
-        this.t = t;
+    /**
+     *
+     * @param term
+     * @param strategy
+     * @return null if not reducible, otherwise reduced Term.
+     */
+    public static Term reduce(Term term, ReductionStrategy strategy) {
+        TreePath path = strategy.getRedex(term);
+        if (path == null) return null;
+        Application app = path.get();
+        SubstitutionVisitor substitutionVisitor = new SubstitutionVisitor(app.getRight());
+        Term substituted = app.getLeft().accept(substitutionVisitor);
+
+        ReplaceVisitor replaceVisitor = new ReplaceVisitor(path.iterator(), substituted);
+        return term.accept(replaceVisitor);
     }
 
+    public static Term reduceN(Term term, ReductionStrategy strategy, int n) {
+        if (n <= 0) throw new IllegalArgumentException();
+        for (; n > 0; n--) {
+            term = reduce(term, strategy);
+            // Presenter will read term here and display it.
+        }
+        return term;
+    }
 
 }
--- src/aurora/shared/backend/TreePath.java
+++ src/aurora/shared/backend/TreePath.java
@@ -1,14 +1,18 @@
 package aurora.shared.backend;
 
+import aurora.shared.backend.tree.Application;
+import aurora.shared.backend.tree.Term;
+
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class TreePath implements Iterable<TreePath.Direction> {
-
     private final LinkedList<Direction> path;
+    private final Term term;
 
-    public TreePath() {
-        this.path = new LinkedList<Direction>();
+    public TreePath(Term term) {
+        this.term = term;
+        this.path = new LinkedList<>();
     }
 
     public void append(Direction d) {
@@ -20,6 +24,10 @@ public class TreePath implements Iterable<TreePath.Direction> {
     @Override
     public Iterator<Direction> iterator() {
         return this.path.iterator();
+    }
+
+    public Application get() {
+        return null;
     }
 
     public enum Direction { LEFT, RIGHT }
