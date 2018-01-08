88da757480493d3d3386011f22f71737d43c05a3
Randy Seng <uidzi@student.kit.edu>
2018-01-08 21:40:13 +0100

    Add CopyToClipBoard Event and Handler

---
+++ src/aurora/client/event/CopyToClipboardEvent.java
@@ -0,0 +1,24 @@
+package aurora.client.event;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class CopyToClipboardEvent extends GwtEvent<CopyToClipboardEventHandler> {
+    public static Type<CopyToClipboardEventHandler> TYPE = new Type<>();
+
+
+    @Override
+    public Type<CopyToClipboardEventHandler> getAssociatedType() {
+        return TYPE;
+    }
+
+    /**
+     * Should only be called by {@link HandlerManager}. In other words, do not use
+     * or call.
+     *
+     * @param handler handler
+     */
+    @Override
+    protected void dispatch(CopyToClipboardEventHandler handler) {
+
+    }
+}
