b3a82027d062a8a9374c5341c2cd8008d555eb01
Max <max.nowak.j@gmail.com>
2018-01-09 18:00:30 +0100

    Add Library fields to EditorPresenter

--- src/aurora/client/presenter/EditorPresenter.java
+++ src/aurora/client/presenter/EditorPresenter.java
@@ -1,6 +1,9 @@
 package aurora.client.presenter;
 
+import aurora.backend.HighlightedLambdaExpression;
+import aurora.backend.library.Library;
 import aurora.client.EditorDisplay;
+import aurora.client.event.StepEvent;
 import aurora.client.event.StepValueChangedEvent;
 import com.google.gwt.event.shared.EventBus;
 
@@ -8,12 +11,23 @@ public class EditorPresenter {
     private final EventBus eventBus;
     private final EditorDisplay editorDisplay;
 
+//    private List<Step> steps;
+    private final Library userLibrary;
+    private final Library standardLibrary;
+
     public EditorPresenter(EventBus eventBus, EditorDisplay editorView) {
         this.eventBus = eventBus;
         this.editorDisplay = editorView;
+        standardLibrary = new Library();
+        userLibrary = new Library();
     }
 
     private void onStepValueChanged(StepValueChangedEvent stepValueChangedEvent) {
         editorDisplay.setStepNumber(stepValueChangedEvent.getStepNumber());
+    }
+
+    private void onStep(StepEvent stepEvent) {
+        HighlightedLambdaExpression hle = /* result of beta reduction */;
+        editorDisplay.addNextStep(hle);
     }
 }
