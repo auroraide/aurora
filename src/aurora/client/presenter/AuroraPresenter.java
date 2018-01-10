fd0b77d8016c67624a3f56ea860fee6d2716e00e
Randy Seng <uidzi@student.kit.edu>
2018-01-10 22:06:27 +0100

    Add JavaDocs for *Display and Aurora EntryPoint Implemented #68

--- src/aurora/client/presenter/AuroraPresenter.java
+++ src/aurora/client/presenter/AuroraPresenter.java
@@ -4,10 +4,26 @@ import aurora.client.AuroraDisplay;
 import aurora.client.event.ExportLaTeXEvent;
 import com.google.gwt.event.shared.EventBus;
 
+/**
+ * <p>
+ *     <code>AuroraPresenter</code> is responsible for the presentation logic.
+ * </p>
+ *
+ * <p>
+ *     It fetches user events and acts upon those by using the backend, which presents the model.
+ *     <code>Aurora Presenter</code> then updates the view through via the {@link AuroraDisplay}.
+ * </p>
+ */
 public class AuroraPresenter {
     private final EventBus eventBus;
     private final AuroraDisplay auroraDisplay;
 
+    /**
+     * Creates an <code>AuroraPresenter</code> with an event bus and a {@link AuroraDisplay}.
+     *
+     * @param eventBus The event bus.
+     * @param auroraDisplay The aurora display.
+     */
     public AuroraPresenter(EventBus eventBus, AuroraDisplay auroraDisplay) {
         this.eventBus = eventBus;
         this.auroraDisplay = auroraDisplay;
--- src/aurora/client/presenter/EditorPresenter.java
+++ src/aurora/client/presenter/EditorPresenter.java
@@ -5,6 +5,17 @@ import aurora.client.EditorDisplay;
 import aurora.client.event.StepEvent;
 import com.google.gwt.event.shared.EventBus;
 
+/**
+ * <p>
+ *     <code>EditorPresenter</code> is responsible for the presentation logic.
+ * </p>
+ *
+ * <p>
+ *     It fetches editor specific user events and acts upon those
+ *     by using the backend, which presents the model. <code>Aurora Presenter</code> then updates the view through
+ *     via the {@link EditorDisplay}.
+ * </p>
+ */
 public class EditorPresenter {
     private final EventBus eventBus;
     private final EditorDisplay editorDisplay;
@@ -13,9 +24,15 @@ public class EditorPresenter {
     private final Library userLibrary;
     private final Library standardLibrary;
 
-    public EditorPresenter(EventBus eventBus, EditorDisplay editorView) {
+    /**
+     * Creates an <code>EditorPresenter</code> with an {@link EventBus} and a {@link EditorDisplay}.
+     *
+     * @param eventBus The event bus.
+     * @param editorDisplay The {@link aurora.client.view.editor.EditorView}
+     */
+    public EditorPresenter(EventBus eventBus, EditorDisplay editorDisplay) {
         this.eventBus = eventBus;
-        this.editorDisplay = editorView;
+        this.editorDisplay = editorDisplay;
         standardLibrary = new Library();
         userLibrary = new Library();
     }
