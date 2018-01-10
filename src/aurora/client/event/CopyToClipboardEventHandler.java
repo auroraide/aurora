9ef0b300e101f9a3083bc26ddbfb746ffac7e3e3
Randy Seng <uidzi@student.kit.edu>
2018-01-10 03:52:47 +0100

    Add missing method stub in CopyToClipboardEventHandler. Remove not needed enum RPCButtonType.

--- src/aurora/client/event/CopyToClipboardEventHandler.java
+++ src/aurora/client/event/CopyToClipboardEventHandler.java
@@ -3,4 +3,5 @@ package aurora.client.event;
 import com.google.gwt.event.shared.EventHandler;
 
 public interface CopyToClipboardEventHandler extends EventHandler {
+    void onCopyToClipboard(CopyToClipboardEvent event);
 }
--- src/aurora/client/view/editor/actionbar/RPCButtonType.java
+++
@@ -1,5 +0,0 @@
-package aurora.client.view.editor.actionbar;
-
-public enum RPCButtonType {
-    RUN, PAUSE, CONTINUE
-}
