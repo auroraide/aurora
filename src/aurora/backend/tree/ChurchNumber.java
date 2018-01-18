e4fac645855f88bfbf06e221b5274fc22ccc6246
Nikolai <uzdgo@student.kit.edu>
2018-01-18 23:10:34 +0100

    added testclass

--- src/aurora/backend/tree/ChurchNumber.java
+++ src/aurora/backend/tree/ChurchNumber.java
@@ -32,5 +32,5 @@ public class ChurchNumber extends Term {
     public Abstraction getAbstraction() {
         return null;
     }
-
+    
 }
---
+++ test/aurora/backend/tree/ChurchNumberTestGenerateAbstraction.java
@@ -0,0 +1,26 @@
+package aurora.backend.tree;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChurchNumberTestGenerateAbstraction {
+
+    @Before
+    public void setUp() throws Exception {
+      ChurchNumber church = new ChurchNumber(4);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAbstraction() {
+       ChurchNumber churchfour = new ChurchNumber(4);
+
+    }
+
+}
