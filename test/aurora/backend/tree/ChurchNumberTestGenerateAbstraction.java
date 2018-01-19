a05496bbae4e868f01d380c5c539dc83bf7d6f29
Nikolai <uzdgo@student.kit.edu>
2018-01-19 01:21:11 +0100

    added one more testcase so i can be sure the code is working as intended. Churchnumber 0-3 are working now every other value should also be correct

--- test/aurora/backend/tree/ChurchNumberTestGenerateAbstraction.java
+++ test/aurora/backend/tree/ChurchNumberTestGenerateAbstraction.java
@@ -26,7 +26,7 @@ public class ChurchNumberTestGenerateAbstraction {
 
     }
     @Test
-    public void testone() throws Exception{
+    public void testone() throws Exception {
     ChurchNumber churchone = new ChurchNumber(1);
     Abstraction a = churchone.getAbstraction();
     Abstraction abody = (Abstraction)a.body;
@@ -38,7 +38,7 @@ public class ChurchNumberTestGenerateAbstraction {
     }
 
     @Test
-    public void testtwo() throws Exception{
+    public void testtwo() throws Exception {
         ChurchNumber churchtwo = new ChurchNumber(2);
         Abstraction a = churchtwo.getAbstraction();
         Abstraction abody = (Abstraction)a.body;
@@ -50,5 +50,24 @@ public class ChurchNumberTestGenerateAbstraction {
         BoundVariable apprightright = (BoundVariable) appright.right;
         assertEquals(2,apprightleft.index);
         assertEquals(1,apprightright.index);
+    }
+    @Test
+    public void testthree() throws Exception {
+        ChurchNumber churchthree = new ChurchNumber(3);
+        Abstraction a = churchthree.getAbstraction();
+        Abstraction abody = (Abstraction)a.body;
+        Application app = (Application)abody.body;
+        BoundVariable left = (BoundVariable)app.left;
+        assertEquals(2,left.index);
+        Application appright = (Application) app.right;
+        BoundVariable apprightleft = (BoundVariable)appright.left;
+        assertEquals(2,apprightleft.index);
+        Application apprightright = (Application) appright.right;
+        BoundVariable apprightrightleft = (BoundVariable)apprightright.left;
+        BoundVariable apprightrightright = (BoundVariable)apprightright.right;
+        assertEquals(2,apprightrightleft.index);
+        assertEquals(1,apprightrightright.index);
+
+
     }
 }
