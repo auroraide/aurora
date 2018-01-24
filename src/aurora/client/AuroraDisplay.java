11648115eef1eb0305111181df44cc932ca6256a
Randy Seng <uidzi@student.kit.edu>
2018-01-24 23:14:53 +0100

    Merge methods of EditorDisplay and SidebarDisplay into AuroraDisplay.

--- src/aurora/client/AuroraDisplay.java
+++ src/aurora/client/AuroraDisplay.java
@@ -1,5 +1,7 @@
 package aurora.client;
 
+import aurora.backend.HighlightedLambdaExpression;
+
 /**
  * The implementation of an Aurora view is being hidden behind the <code>AuroraDisplay</code> interface.
  * <p>
@@ -29,6 +31,81 @@ public interface AuroraDisplay {
      * @param stepNumber The step number.
      */
     void setStepNumber(int stepNumber);
+
+    /**
+     * Displays a syntax error message.
+     *
+     * @param message Message to display.
+     */
+    void displaySyntaxError(String message); // TODO there's more than one way to display bad results.
+
+    /**
+     * Gets the user input String from the code editor.
+     *
+     * @return User input. Unparsed lambda term.
+     */
+    String getInput();
+
+    /**
+     * Sets the content of the code editor, replacing it entirely.
+     *
+     * @param highlightedLambdaExpression term with syntax highlighting.
+     */
+    void setInput(HighlightedLambdaExpression highlightedLambdaExpression); // TODO umbenenny, staeamynecheine
+
+    /**
+     * Appends a new step to the current step list.
+     *
+     * @param highlightedLambdaExpression Step to append.
+     */
+    void addNextStep(HighlightedLambdaExpression highlightedLambdaExpression);
+
+    /**
+     * Wipes the step list entirely.
+     */
+    void resetSteps();
+
+    /**
+     * Displays the result and locks any outputs. No further steps can be added after calling this.
+     *
+     * @param highlightedLambdaExpression Result to display.
+     */
+    void displayResult(HighlightedLambdaExpression highlightedLambdaExpression);
+
+    /**
+     * Closes an add function dialog form.
+     */
+    void closeAddLibraryItemDialog();
+
+    /**
+     * Adds a new function to the views user library.
+     *
+     * @param name        The name function.
+     * @param description The function description.
+     */
+    void addUserLibraryItem(String name, String description);
+
+    /**
+     * Removes a function from the view's user library.
+     *
+     * @param name The name of the function to be removed.
+     */
+    void removeUserLibraryItem(String name);
+
+    /**
+     * Adds a new function to the views' standard library.
+     *
+     * @param name        The name of the function to be added.
+     * @param description The function description.
+     */
+    void addStandardLibraryItem(String name, String description);
+
+    /**
+     * Removes a function form the views' standard library.
+     *
+     * @param name The name of the function to be removed.
+     */
+    void removeStandardLibraryItem(String name);
 
 
 }
