9a933a17c1cb9b131d0c018bb91c31f4bdc0fedf
Max <max.nowak.j@gmail.com>
2018-01-25 03:50:14 +0100

    Basic untested run/pause/stop first attempts.

--- src/aurora/client/presenter/EditorPresenter.java
+++ src/aurora/client/presenter/EditorPresenter.java
@@ -2,14 +2,22 @@ package aurora.client.presenter;
 
 import aurora.backend.HighlightableLambdaExpression;
 import aurora.backend.HighlightedLambdaExpression;
-import aurora.backend.RedexPath;
 import aurora.backend.betareduction.BetaReducer;
-import aurora.backend.betareduction.strategies.ReductionStrategy;
+import aurora.backend.betareduction.BetaReductionIterator;
+import aurora.backend.betareduction.strategies.*;
 import aurora.backend.library.Library;
+import aurora.backend.parser.LambdaLexer;
+import aurora.backend.parser.LambdaParser;
+import aurora.backend.parser.exceptions.SemanticException;
+import aurora.backend.parser.exceptions.SyntaxException;
+import aurora.backend.tree.Term;
 import aurora.client.AuroraDisplay;
-import aurora.client.event.StepEvent;
+import aurora.client.event.*;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.Timer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <code>EditorPresenter</code> is responsible for the presentation logic.
@@ -20,13 +28,24 @@ import com.google.gwt.user.client.Timer;
 public class EditorPresenter {
     private final AuroraDisplay auroraDisplay;
 
-    //    private List<Step> steps;
     private final Library userLibrary;
     private final Library standardLibrary;
 
     private final LambdaLexer lambdaLexer;
     private final LambdaParser lambdaParser;
 
+    /**
+     * Contains all steps in between, so without input and without output fields.
+     */
+    private final List<Term> steps;
+
+    /**
+     * GWT Timer, allows for "while" loops without blocking the GUI.
+     */
+    private final RunTimer runTimer;
+
+    private BetaReductionIterator betaReductionIterator;
+    private Term last;
     private ReductionStrategy reductionStrategy;
 
     /**
@@ -34,55 +53,122 @@ public class EditorPresenter {
      *
      * @param auroraDisplay The {@link AuroraDisplay}
      */
-    public EditorPresenter(AuroraDisplay auroraDisplay) {
+    public EditorPresenter(EventBus eventBus, AuroraDisplay auroraDisplay) {
         this.auroraDisplay = auroraDisplay;
         standardLibrary = new Library();
         userLibrary = new Library();
+        steps = new ArrayList<>();
+        runTimer = new RunTimer();
 
-        bind();
+        bind(eventBus);
         lambdaLexer = new LambdaLexer();
         lambdaParser = new LambdaParser();
+
+        reset();
     }
 
-    private void bind() {
+    private void bind(EventBus eventBus) {
         eventBus.addHandler(RunEvent.TYPE, runEvent -> onRun());
         eventBus.addHandler(StepEvent.TYPE, this::onStep);
-        eventBus.addHandler(ResetEvent.TYPE, this::onReset);
-        eventBus.addHandler(PauseEvent.TYPE, this::onPause);
+        eventBus.addHandler(ResetEvent.TYPE, runEvent1 -> onReset());
+        eventBus.addHandler(PauseEvent.TYPE, runEvent -> onPause());
+        eventBus.addHandler(EvaluationStrategyChangedEvent.TYPE, this::onStrategyChange);
+    }
+
+    private void reset() {
+        betaReductionIterator = null;
+        last = null;
+        reductionStrategy = null;
+    }
+
+    private class RunTimer extends Timer {
+        @Override
+        public void run() {
+            if (betaReductionIterator.hasNext()) {
+                betaReductionIterator.next();
+            }
+            else {
+                auroraDisplay.displayResult(new HighlightableLambdaExpression(last));
+                reset();
+                cancel();
+            }
+        }
     }
 
     /**
      * Before entering this, we can only be in the default state.
+     * Reminder: Run doesn't show the steps, only starts evaluation in the background until completion.
      */
     private void onRun() {
         // view changes its state by itself.
-        String input = editorDisplay.getInput();
+        String input = auroraDisplay.getInput();
+
+        // first up, parse the input and display it in the editor for highlighting premium.
         try {
-            Term term = lambdaParser.parse(lambdaLexer.lex(input));
-            HighlightedLambdaExpression hle = new HighlightableLambdaExpression(term);
-            editorDisplay.setInput(hle);
-            
-            BetaReducer br = new BetaReducer(reductionStrategy);
-            Term result = br.reduce(term);
-            if (result == null) {
-                // TODO display result, end
-                editorDisplay.displayResult();
-            } else {
-                editorDisplay.addNextStep(new HighlightableLambdaExpression(result));
-            }
-        }
-        catch (SyntaxException | SemanticException ex) {
+            last = lambdaParser.parse(lambdaLexer.lex(input));
+        } catch (SyntaxException | SemanticException ex) {
             // TODO actually handle input errors.
             throw new RuntimeException();
         }
+        HighlightedLambdaExpression hle = new HighlightableLambdaExpression(last);
+        auroraDisplay.setInput(hle);
+
+        betaReductionIterator = new BetaReductionIterator(new BetaReducer(reductionStrategy), last);
+        if (!betaReductionIterator.hasNext()) {
+            // term is irreducible.
+            auroraDisplay.displayResult(new HighlightableLambdaExpression(last));
+        }
+        else {
+            runTimer.scheduleRepeating(0);
+        }
     }
-    private void onPause(PauseEvent runEvent) {
+    private void onPause() {
+        assert (runTimer != null);
+        runTimer.cancel();
+        if (!betaReductionIterator.hasNext()) {
+            // this is a corner case, and in most cases won't happen.
+            auroraDisplay.displayResult(new HighlightableLambdaExpression(last));
+            reset();
+        } else {
+            // TODO display that the steps list is potentially incomplete
+            for (int i = Math.min(0, steps.size() - 10); i < steps.size(); i++) {
+                auroraDisplay.addNextStep(new HighlightableLambdaExpression(steps.get(i)));
+            }
+        }
     }
-    private void onReset(ResetEvent runEvent) {
+    private void onReset() {
+        betaReductionIterator = null;
+        runTimer.cancel();
+        steps.clear();
     }
 
     private void onStep(StepEvent stepEvent) {
-        // HighlightableLambdaExpression hle = /* result of beta reduction */;
-        // editorDisplay.addNextStep(hle);
+        assert (betaReductionIterator.hasNext());
+        Term result = betaReductionIterator.next();
+        HighlightableLambdaExpression hle = new HighlightableLambdaExpression(result);
+        if (betaReductionIterator.hasNext()) {
+            auroraDisplay.addNextStep(hle);
+        } else {
+            auroraDisplay.displayResult(hle);
+        }
+    }
+
+    private void onStrategyChange(EvaluationStrategyChangedEvent strat) {
+        switch (strat.getStrategyType()) {
+
+            case CALLBYVALUE:
+                reductionStrategy = new CallByValue();
+                break;
+            case CALLBYNAME:
+                reductionStrategy = new CallByName();
+                break;
+            case NORMALORDER:
+                reductionStrategy = new NormalOrder();
+                break;
+            case MANUALSELECTION:
+                throw new RuntimeException(); // TODO implement UserStrategy
+//                reductionStrategy = new UserStrategy();
+        }
+
     }
 }
