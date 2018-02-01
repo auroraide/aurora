94fa0cc65eb51ed937246af2eb318284946206de
Alexander von Heyden <alexandervonheyden@gmx.net>
2018-02-01 04:33:46 +0100

    Added lambda to aurorascript highlighter

--- war/codemirror/mode/aurorascript/aurorascript.js
+++ war/codemirror/mode/aurorascript/aurorascript.js
@@ -3,7 +3,8 @@ CodeMirror.defineSimpleMode("aurorascript", {
         {regex: /#[^\n]*\n*/, token: "comment"},
         {regex: /\$[a-z]+/, token: "keyword"},
         {regex: /[0-9]+/, token: "number"},
-        {regex: /[a-z]+/, token: "variable-2"}
+        {regex: /[a-z]+/, token: "variable-2"},
+        {regex: /λ/, token: "atom"}
     ],
     meta: {
         lineComment: "#"
