a133f955a9abf06a09b04b2cec331bd29c9bebe7
Nikolai <uzdgo@student.kit.edu>
2018-02-07 22:29:23 +0100

    added sharelatex

---
+++ test/aurora/backend/ShareLaTeXTest.java
@@ -0,0 +1,35 @@
+package aurora.backend;
+
+import aurora.backend.tree.Abstraction;
+import aurora.backend.tree.BoundVariable;
+import aurora.backend.tree.FreeVariable;
+import aurora.backend.tree.Function;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShareLaTeXTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testlambdasymbol() {
+        HighlightableLambdaExpression hle = new HighlightableLambdaExpression(new Abstraction(new BoundVariable(1),"x"));
+        ShareLaTeX sh = new ShareLaTeX(hle);
+        assertEquals(sh.generateLaTeX(),"$\\lambda$ x . 1 ");
+    }
+    @Test
+    public void dollar() {
+        HighlightableLambdaExpression hle = new HighlightableLambdaExpression(new Function("test", new FreeVariable("x")));
+        ShareLaTeX sh = new ShareLaTeX(hle);
+        assertEquals(sh.generateLaTeX(), "\\$test ");
+    }
+}
