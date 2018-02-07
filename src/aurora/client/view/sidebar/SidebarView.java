83e65dc73e7266427413aabb363018b200ebddc3
Juli Patrusheva <ip.tiamo@gmail.com>
2018-02-07 09:48:03 +0100

    fix checktyle ONCE AGAIN (Niko)

--- src/aurora/client/view/sidebar/SidebarView.java
+++ src/aurora/client/view/sidebar/SidebarView.java
@@ -16,7 +16,12 @@ import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.IntegerBox;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Widget;
 
 
 /**
