8d5e5a90d62bc1a712109869518e16ac3dab2682
Max <max.nowak.j@gmail.com>
2018-02-08 19:55:02 +0100

    Add ReStepEvent

---
+++ src/aurora/client/event/ReStepEventHandler.java
@@ -0,0 +1,7 @@
+package aurora.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface ReStepEventHandler extends EventHandler {
+    void onReStepEvent(ReStepEvent reStepEvent);
+}
