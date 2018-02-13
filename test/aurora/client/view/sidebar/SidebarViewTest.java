44d97243f447be4a7dd40cea6d1b26d5502d3449
Randy Seng <randy.seng@yahoo.com>
2018-02-13 15:50:44 +0100

    Add testcase for SidebarView.

---
+++ test/aurora/client/view/sidebar/SidebarViewTest.java
@@ -0,0 +1,22 @@
+package aurora.client.view.sidebar;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * Test SidebarView.
+ */
+public class SidebarViewTest extends GWTTestCase {
+    public String getModuleName() {
+        return "aurora.Aurora";
+    }
+
+    /**
+     * Sdfl.
+     */
+    public void testSidebar() {
+        String yo = "yo";
+        assertEquals("yo", yo);
+
+    }
+
+}
