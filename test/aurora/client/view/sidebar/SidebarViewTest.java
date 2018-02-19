6a3d03197492aa90f011bbc157475843741c8dbc
Randy Seng <randy.seng@yahoo.com>
2018-02-20 00:22:44 +0100

    Make GWTTestCase work when running ant test.

--- test/aurora/client/view/sidebar/SidebarViewTest.java
+++ test/aurora/client/view/sidebar/SidebarViewTest.java
@@ -2,12 +2,22 @@ package aurora.client.view.sidebar;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
+
+
+
 /**
  * Test SidebarView.
  */
 public class SidebarViewTest extends GWTTestCase {
+    private SidebarView sidebarView;
+
     public String getModuleName() {
         return "aurora.Aurora";
+    }
+
+
+    public void gwtSetUp() {
+
     }
 
     /**
