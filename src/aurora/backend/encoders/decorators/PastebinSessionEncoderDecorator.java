ba54c444105170508909ce38e595af10a4d1d994
Alexander von Heyden <alexandervonheyden@gmx.net>
2018-03-06 00:43:50 +0100

    Now using gist.github instead of pastebin, just works

--- src/aurora/backend/encoders/decorators/PastebinSessionEncoderDecorator.java
+++ src/aurora/backend/encoders/decorators/PastebinSessionEncoderDecorator.java
@@ -1,5 +1,20 @@
 package aurora.backend.encoders.decorators;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+
 import aurora.backend.encoders.SessionEncoder;
 import aurora.backend.encoders.exceptions.DecodeException;
 
@@ -9,7 +24,7 @@ import aurora.backend.encoders.exceptions.DecodeException;
  */
 public class PastebinSessionEncoderDecorator extends SessionEncoderDecorator {
 
-    protected PastebinSessionEncoderDecorator() {
+    public PastebinSessionEncoderDecorator() {
         // This page is intentionally left blank.
     }
 
@@ -37,6 +52,54 @@ public class PastebinSessionEncoderDecorator extends SessionEncoderDecorator {
 
         // ...then decode it like normal people
         return this.sessionEncoder.decode(toBeDecoded);
+    }
+
+
+    public String testPaste(String code) {
+        return new Paste(code).upload();
+    }
+
+
+
+    private class Paste {
+        private static final String POST_URL = "https://api.github.com/gists";
+        private static final String API_KEY = "e09a05a00409086d72e3dfae85d341c8";
+        
+        private String code;
+        public String url = null;
+
+        public Paste(String code) {
+            this.code = code;
+        }
+        
+        private native void console(String message) /*-{
+            console.log(message);
+        }-*/;
+    
+        private String upload() {
+            RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, POST_URL);
+            try {
+                Request response = builder.sendRequest(
+                        "{ \"files\" :{"
+                            + "\"file1.txt\": {"
+                                + "\"content\": \"File content\""
+                            + "}"
+                        + "}}",
+                        new RequestCallback() {
+                    public void onError(Request request, Throwable exception) {
+                        throw new RuntimeException("Error reveivung from Pastebin...");
+                    }
+                    public void onResponseReceived(Request request, Response response) {
+                        console(response.getText().toString());
+                    }
+                });
+            } catch (RequestException e) {
+                throw new RuntimeException("Error Pastebin");
+            }
+            return "Banana";
+
+        }
+
     }
 
 }
