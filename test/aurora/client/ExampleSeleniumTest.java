7e3fb4b9c2291aee62a486530b89c36aafb78976
Randy Seng <randy.seng@yahoo.com>
2018-03-10 16:47:57 +0100

    Update Javadoc.

--- test/aurora/client/ExampleSeleniumTest.java
+++ test/aurora/client/ExampleSeleniumTest.java
@@ -9,8 +9,8 @@ import com.machinepublishers.jbrowserdriver.Settings;
 import static junit.framework.TestCase.assertTrue;
 
 /**
- * An example test using selenium as instructed by this youtube video:
- * <a href="https://www.youtube.com/watch?v=nqaY0UgRcFQ">
+ * An example test using selenium.
+ *
  * "How to create a simple Selenium WebDriver test using Java and IntelliJ"</a>.
  */
 public class ExampleSeleniumTest {
