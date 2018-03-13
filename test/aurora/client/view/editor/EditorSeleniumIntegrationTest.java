b3a91a459632a810f086781ff2978788025f0dae
Randy Seng <randy.seng@yahoo.com>
2018-03-13 19:22:25 +0100

    Implement test cases of T4

--- test/aurora/client/view/editor/EditorSeleniumIntegrationTest.java
+++ test/aurora/client/view/editor/EditorSeleniumIntegrationTest.java
@@ -7,11 +7,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class EditorSeleniumIntegrationTest {
     private final String pathToEditor = "//*[@id=\"inputCodeMirror\"]/div/div[1]/textarea";
@@ -41,72 +43,63 @@ public class EditorSeleniumIntegrationTest {
     }
 
     /**
-     * Tests T4.1
-     *
-     * Precondition: Aurora WebApp is opened and in DefaultState.
-     * Action: The User types in the lambda term "λx.x" in the input code editor and clicks on the run button.
-     * Reaction: The programm can't execute a beta-reduction and outputs "" in the result field.
+     * Tests T4.1 (See Pflichtenheft).
      */
     @Test
     public void testIrreducibleLambdaTerm() {
         codeEditor.sendKeys("λx.x");
         driver.findElement(By.id("runButton")).click();
-        String resultContent = driver.findElements(By.tagName("pre")).get(1).getText();
-        assertEquals("λx. x", resultContent);
+        String result = driver.findElement(By.id("outputCodeMirror")).findElements(By.tagName("pre")).get(0).getText();
+        assertEquals("λx. x", result);
     }
 
     /**
-     * Tests T4.2
-     *
-     * Precondition: Aurora WebApp is opened and in DefaultState.
-     * Action: The User types in the lambda term "(λx.x x) z" in the input code editor and clicks on the run button.
-     * Reaction: The programm executes one beta-reduction and outputs "z" in the result field.
+     * Tests T4.2 (See Pflichtenheft).
      */
     @Test
     public void testOneBetaReduction() {
         codeEditor.sendKeys("(λx.x) z");
         driver.findElement(By.id("runButton")).click();
-        String resultContent = driver.findElements(By.tagName("pre")).get(2).getText();
-        assertEquals("z", resultContent);
+        String result = driver.findElement(By.id("outputCodeMirror")).findElements(By.tagName("pre")).get(0).getText();
+        assertEquals("z", result);
     }
 
     /**
-     * Tests T4.3
-     *
-     * Precondition: Aurora WebApp is opened and in DefaultState.
-     * Action: The User types in the lambda term "(λn.λm.λs.λz.n s (m s z)) (λs.λz.s(s z)) (λs.λz.s(s z))"
-     *         in the input code editor and clicks on the run button.
-     * Reaction: The programm executes one beta-reduction and outputs "4" in the result field.
+     * Tests T4.3 (See Pflichtenheft).
      */
     @Test
     public void testPlusTwoTwoReduction() {
         codeEditor.sendKeys("(λn.λm.λs.λz.n s (m s z)) (λs.λz.s(s z)) (λs.λz.s(s z))");
         driver.findElement(By.id("runButton")).click();
-        String resultContent = driver.findElements(By.tagName("pre")).get(2).getText();
-        assertEquals("4", resultContent);
+        String result = driver.findElement(By.id("outputCodeMirror")).findElements(By.tagName("pre")).get(0).getText();
+        assertEquals("4", result);
     }
 
     /**
-     * Tests T4.5
-     *
-     * Precondition: Aurora WebApp is calculating a lambda term and therefore is in RunningState.
-     * Action: The user clicks on the reset button.
-     * Reaction: The programm terminates the current calculation and does not output anything.
+     * Tests T4.5 (See Pflichtenheft).
      */
     @Test
     public void testNoResultOnResetBtnClicked() {
         codeEditor.sendKeys("$pow 5 5");
         driver.findElement(By.id("runButton")).click();
-        int resultContent = driver.findElements(By.tagName("pre")).get(1).getText().hashCode();
-        assertEquals(8203, resultContent);
+        int result = driver.findElement(By.id("outputCodeMirror")).findElements(By.tagName("pre"))
+                .get(0).getText().hashCode();
+        assertEquals(8203, result);
     }
 
     /**
-     * Tests T4.7
-     *
-     * Precondition: Aurora WebApp is calculating a lambda term and therefore is in RunningState.
-     * Action: The user clicks on the pause button.
-     * Reaction:
+     * Tests T4.6 (See Pflichtenheft.)
+     */
+    @Test
+    public void testEditingInEditorNotPossible() {
+        codeEditor.sendKeys("(λ.x x) (λx.x x)");
+        driver.findElement(By.id("runButton")).click();
+        codeEditor.click();
+        assertFalse("Is code editor selected", codeEditor.isSelected());
+    }
+
+    /**
+     * Tests T4.7 (See Pflichtenheft).
      */
     @Test
     public void testContinueButtonClickedPossible() {
@@ -118,7 +111,52 @@ public class EditorSeleniumIntegrationTest {
                 && driver.findElement(By.id("continueButton")).isEnabled();
 
         assertTrue("Continue button is displayed and visible", continueBtnDisplayedAndEnabled);
+    }
 
+    /**
+     * Tests T4.8 (See Pflichtenheft).
+     * TODO write tests when displaying error message dialogbox in editorview is implemented.
+     */
+    @Test
+    public void testErrorMessageDisplayedByWrongInput() {
 
+    }
+
+    /**
+     * Tests T4.9 (See Pflichtenheft).
+     */
+    @Test
+    public void testBraceAutoComplete() {
+        codeEditor.sendKeys("(");
+        // document.getElementById("inputCodeMirror").getElementsByTagName("pre")[0].textContent
+        String editorContent = "" + driver.findElement(By.id("inputCodeMirror")).findElements(By.tagName("pre"))
+                .get(1).getText();
+        assertEquals("()", editorContent);
+    }
+
+    /**
+     * Tests T4.10 (See Pflichtenheft).
+     */
+    @Test
+    public void testResultFieldNotEditable() {
+        WebElement resultField = driver.findElement(By.id("outputCodeMirror"))
+                .findElements(By.tagName("pre")).get(0);
+        resultField.click();
+        assertFalse("Result field should not be clickable", resultField.isSelected());
+    }
+
+    /**
+     * Tests T4.11 (See Pflichtenheft).
+     */
+    @Test
+    public void testCommentsIgnoredInCalculation() {
+        codeEditor.sendKeys("#dies ist ein test (λz.z) a");
+        codeEditor.sendKeys(Keys.RETURN);
+        System.out.println(driver.findElement(By.id("inputCodeMirror")).
+                findElements(By.tagName("pre")).get(0).getText());
+        codeEditor.sendKeys("(λ x. x) y");
+        driver.findElement(By.id("runButton")).click();
+        String result = driver.findElement(By.id("outputCodeMirror")).findElements(By.tagName("pre")).get(0).getText();
+        assertEquals("z", result);
     }
 }
