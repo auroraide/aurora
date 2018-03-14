9b1d7d90b1f732959e81d3ec9b84c97e047ad7a8
Max <max.nowak.j@gmail.com>
2018-03-14 20:07:58 +0100

    Regression test #178

---
+++ test/aurora/client/presenter/SidebarPresenterTest.java
@@ -0,0 +1,56 @@
+package aurora.client.presenter;
+
+import aurora.backend.library.HashLibrary;
+import aurora.backend.parser.LambdaLexer;
+import aurora.backend.parser.LambdaParser;
+import aurora.backend.simplifier.ChurchNumberSimplifier;
+import aurora.backend.simplifier.LibraryTermSimplifier;
+import aurora.client.EditorDisplay;
+import aurora.client.SidebarDisplay;
+import aurora.client.event.AddFunctionEvent;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.SimpleEventBus;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SidebarPresenterTest {
+    private static LambdaParser parser;
+    private static LambdaLexer lexer;
+
+    private SidebarDisplay sidebarDisplay;
+    private EventBus bus;
+    private SidebarPresenter sidebarPresenter;
+
+    @BeforeClass
+    public static void setUpClass() {
+        lexer = new LambdaLexer();
+        parser = new LambdaParser(new HashLibrary());
+    }
+
+    @Before
+    public void setUp() {
+        sidebarDisplay = mock(SidebarDisplay.class);
+        bus = new SimpleEventBus();
+        sidebarPresenter = new SidebarPresenter(
+                bus,
+                sidebarDisplay,
+                new HashLibrary(),
+                new HashLibrary(),
+                lexer,
+                parser
+        );
+    }
+
+    @Test
+    public void regressionTest178AddingFunctionnameWithWhitespaceWorksBug() {
+        bus.fireEvent(new AddFunctionEvent("hello there", "x", "descr"));
+        verify(sidebarDisplay).displayAddLibraryItemInvalidName();
+    }
+}
