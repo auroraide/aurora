b75dcc76ce7a41675935bca7f17d4f88bb3ebf27
Randy Seng <randy.seng@yahoo.com>
2018-03-16 03:10:13 +0100

    Remove @Before method, because it makes the SeleniumTests which are inheriting this class KAPUTT

--- test/aurora/utils/SeleniumTemplate.java
+++ test/aurora/utils/SeleniumTemplate.java
@@ -3,16 +3,12 @@ package aurora.utils;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class SeleniumTemplate {
     protected final String pathToEditor = "//*[@id=\"inputCodeMirror\"]/div/div[1]/textarea";
     protected static WebDriver driver;
-    protected WebElement codeEditor;
 
     /**
      * Starts the webdriver.
@@ -25,15 +21,6 @@ public class SeleniumTemplate {
     @AfterClass
     public static void closeWebDriver() {
         driver.quit();
-    }
-
-    /**
-     * Sets up the testing environment.
-     */
-    @Before
-    public void setUp() {
-        driver.navigate().to("http://localhost:4000");
-        codeEditor = driver.findElement(By.xpath(pathToEditor));
     }
 
 
