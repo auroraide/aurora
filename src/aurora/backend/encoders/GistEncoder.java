34821b38b6faab56687e347d6492f91f5543cf3b
Alexander von Heyden <alexandervonheyden@gmx.net>
2018-03-16 07:18:33 +0100

    Started rewriting GistEncoder

---
+++ src/aurora/backend/encoders/GistEncoder.java
@@ -0,0 +1,14 @@
+package aurora.backend.encoders;
+
+import aurora.backend.encoders.exceptions.DecodeException;
+import aurora.backend.library.Library;
+
+/**
+ * GIST Encoder and Decoder.
+ * Saves the current input and library items in a GIST, and returns the URL.
+ * Given a valid URL, it returns input as well as the decoded library items.
+ */
+public class GistEncoder {
+
+}
+
