a1b0b11099258919aa45f0b732e9bc887ab5d973
Randy Seng <randy.seng@yahoo.com>
2018-03-19 03:50:38 +0100

    Add integration test for sidebar

--- test/aurora/client/view/editor/AddUserLibSeleniumTest.java
+++ test/aurora/client/view/editor/AddUserLibSeleniumTest.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -16,9 +15,8 @@ import org.openqa.selenium.support.ui.Wait;
 import java.time.Duration;
 import java.util.NoSuchElementException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AddUserLibSeleniumTest {
     private static WebDriver driver;
@@ -72,6 +70,10 @@ public class AddUserLibSeleniumTest {
         return driver.findElement(By.id("outputCodeMirror")).findElements(By.tagName("pre")).get(1).getText();
     }
 
+    private WebElement getStepButton() {
+        return driver.findElement(By.id("stepButton"));
+    }
+
     private String waitForOutput() {
         Wait<WebDriver> waiter = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(20))
@@ -105,6 +107,46 @@ public class AddUserLibSeleniumTest {
      */
     @Test
     public void testFirstPair() {
+        waitForElemExists(By.id("addFunctionButton")).click();
+        waitForElemExists(By.id("nameField")).sendKeys("first2");
+        waitForElemExists(By.id("functionField")).sendKeys("λp.p(λa.λb.a)");
+        waitForElemExists(By.id("addButton")).click();
 
+        getInput().sendKeys("$first2 ($pair a b)");
+        getRunButton().click();
+
+        assertEquals("a", getOutput());
     }
+
+    /**
+     * T6.4
+     */
+    @Test
+    public void removeFirst() {
+        waitForElemExists(By.id("addFunctionButton")).click();
+        waitForElemExists(By.id("nameField")).sendKeys("first2");
+        waitForElemExists(By.id("functionField")).sendKeys("λp.p(λa.λb.a)");
+        waitForElemExists(By.id("addButton")).click();
+
+        getInput().sendKeys("$first2 ($pair a b)");
+        getRunButton().click();
+
+        driver.findElement((By.id("removeLibraryItemButton-0"))).click();
+
+        assertTrue(driver.findElements(By.id("removeLibraryItemButton-0")).size() == 0);
+    }
+
+    /**
+     * T6.5 T6.6
+     */
+    @Test
+    public void testUseStdlibFunctionAddTwoTwo() {
+        getInput().sendKeys("$add 2 2");
+        getRunButton().click();
+        assertEquals("4", getOutput());
+    }
+
+
+
+
 }
