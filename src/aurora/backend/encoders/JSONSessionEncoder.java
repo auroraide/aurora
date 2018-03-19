e2db0fa3f3775f263a3716649d7ba39ab15b24a5
Alexander von Heyden <alexandervonheyden@gmx.net>
2018-03-19 01:23:54 +0100

    Now using GWT JSON* to ex/import JSON

--- src/aurora/backend/encoders/JSONSessionEncoder.java
+++ src/aurora/backend/encoders/JSONSessionEncoder.java
@@ -1,9 +1,5 @@
 package aurora.backend.encoders;
 
-import com.google.gwt.core.client.JsonUtils;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsArrayMixed;
-
 import aurora.backend.parser.exceptions.SemanticException;
 import aurora.backend.parser.exceptions.SyntaxException;
 import aurora.backend.parser.LambdaLexer;
@@ -15,6 +11,17 @@ import aurora.backend.library.HashLibrary;
 import aurora.backend.library.MultiLibrary;
 import aurora.backend.encoders.exceptions.DecodeException;
 import aurora.backend.encoders.JSONEscaper;
+import aurora.backend.parser.Token;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONException;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 /**
  * Serialize/deserialize a {@link Session} into a JSON string.
@@ -52,19 +59,21 @@ public class JSONSessionEncoder extends SessionEncoder {
 
     @Override
     public String encode(Session session) {
-        JavaScriptObject jso = JavaScriptObject.createObject();
-        setProperty(jso, "rawInput", session.rawInput);
+        JSONObject jo = new JSONObject();
+        jo.put("rawInput", new JSONString(session.rawInput));
 
-        JsArrayMixed lib = JavaScriptObject.createArray().cast();
+        JSONArray library = new JSONArray();
         session.library.forEach(
-                item -> lib.push(
-                    addLibraryEntry(
-                        item.getName(),
-                        item.getDescription(),
-                        new HighlightableLambdaExpression(item.getTerm()).toString())));
-        setProperty(jso, "library", lib);
-
-        return JsonUtils.stringify(jso);
+                item -> {
+                    JSONArray libraryItem = new JSONArray();
+                    libraryItem.set(0, new JSONString(item.getName()));
+                    libraryItem.set(1, new JSONString(item.getDescription()));
+                    libraryItem.set(2, new JSONString(
+                                new HighlightableLambdaExpression(item.getTerm()).toString()));
+                    library.set(library.size(), libraryItem);
+                });
+        jo.put("library", library);
+        return jo.toString();
     }
 
     /**
@@ -80,28 +89,56 @@ public class JSONSessionEncoder extends SessionEncoder {
 
     @Override
     public Session decode(String encodedInput) throws DecodeException {
-        String toDecode = JsonUtils.escapeJsonForEval(encodedInput);
-        if (!JsonUtils.safeToEval(encodedInput)) {
-            throw new DecodeException("Invalid json file");
-        }
-        JavaScriptObject jso = JsonUtils.safeEval(encodedInput);
-        //TODO Try catch rawInput
-        String rawInput = getProperty(jso, "rawInput");
-        //TODO Try catch this one
-        String[][] libraryString = getLibrary(jso);
-        
-        String name;
-        String description;
-        Term term;
+        JSONObject value = null;
+        String rawInput = null;
+        JSONArray encodedLibrary = null;
+        Library userLin = new HashLibrary();
 
-        for (int i = 0; i < libraryString.length; ++i) {
-            name = libraryString[i][0];
-            description = libraryString[i][1];
+        try {
+            value = JSONParser.parseStrict(encodedInput).isObject();
+        } catch (JSONException e) {
+            throw new DecodeException(e.getMessage());
+        }
+
+        try {
+            rawInput = value.get("rawInput").toString(); 
+        } catch (JSONException e) {
+            throw new DecodeException(e.getMessage());
+        }
+
+        try {
+           encodedLibrary = (JSONArray) value.get("library").isArray();
+        } catch (JSONException e) {
+            throw new DecodeException(e.getMessage());
+        }
+
+        for (int i = 0; i < encodedLibrary.size(); ++i) {
+            JSONArray userLibData = (JSONArray) encodedLibrary.get(i);
+            final String name = userLibData.get(0).isString().stringValue();
+            final String function = userLibData.get(1).isString().stringValue();
+            final String description = userLibData.get(2).isString().stringValue();
+
+            Term term;
             try {
-                term = lambdaParser.parse(lambdaLexer.lex(libraryString[i][2]));
-            } catch (SemanticException | SyntaxException e) {
-                throw new DecodeException("Invalid json file: " + e.getMessage());
-            } 
+                term = lambdaParser.parse(lambdaLexer.lex(function));
+            } catch (SyntaxException | SemanticException e) {
+                throw new DecodeException(e.getMessage());
+            }
+
+            List<Token> tokens;
+            try {
+                tokens = lambdaLexer.lex("$" + name);
+            } catch (SyntaxException e) {
+                throw new DecodeException(e.getMessage());
+            }
+            if (tokens.size() != 1 || tokens.get(0).getType() != Token.TokenType.T_FUNCTION) {
+                throw new DecodeException(name + "is invalid function name");
+            }
+
+            if ((new MultiLibrary(stdLib, userLib).exists(name))) {
+                throw new DecodeException(name + "may only exist once");
+            }
+
             userLib.define(name, description, term);
         }
 
@@ -119,33 +156,33 @@ public class JSONSessionEncoder extends SessionEncoder {
         return jsonEscaper.unescape(toUnescape);
     }
 
-    private native void setProperty(JavaScriptObject jso, String property, Object value) /*-{
-        jso[property] = value;
-    }-*/;
+    //private native void setProperty(JavaScriptObject jso, String property, Object value) /*-{
+    //    jso[property] = value;
+    //}-*/;
 
-    private native JsArrayMixed addLibraryEntry(String name, String description, Term term) /*-{
-        return [name, description, term];
-    }-*/;
+    //private native JsArrayMixed addLibraryEntry(String name, String description, Term term) /*-{
+    //    return [name, description, term];
+    //}-*/;
 
-    private native JsArrayMixed addLibraryEntry(String name, String description, String term) /*-{
-        return [name, description, term];
-    }-*/;
+    //private native JsArrayMixed addLibraryEntry(String name, String description, String term) /*-{
+    //    return [name, description, term];
+    //}-*/;
 
-    private native String getProperty(JavaScriptObject jso, String property) /*-{
-        return jso[property];
-    }-*/;
+    //private native String getProperty(JavaScriptObject jso, String property) /*-{
+    //    return jso[property];
+    //}-*/;
 
-    private native String[][] getLibrary(JavaScriptObject jso) /*-{
-        return jso["library"];
-    }-*/;
+    //private native String[][] getLibrary(JavaScriptObject jso) /*-{
+    //    return jso["library"];
+    //}-*/;
 
-    private native void readLibrary(JavaScriptObject jso, Library library) /*-{
+    //private native void readLibrary(JavaScriptObject jso, Library library) /*-{
 
-    }-*/;
+    //}-*/;
 
-    private native void console(String message) /*-{
-        console.log(message);
-    }-*/;
+    //private native void console(Object message) /*-{
+    //    console.log(message);
+    //}-*/;
 
 }
 
